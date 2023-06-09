76c185ca527b3fd3d212a590d0d8a4d9d27517bd
angela <angelali6462@gmail.com>
2023-06-08 22:18:07 -0700

    added profile + username

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
-import { Login } from './Login';
+import Login from './Login';
 import RecipeCard from './recipeCard';
+import { connect } from 'react-redux'
 
-function App() {
+function App({dispatch, username}) {
   return (
     <div className='Frontpage'>
       <div className="App">
@@ -10,8 +11,20 @@ function App() {
         <RecipeCard title="TestRecipe" image="https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fpublic-assets.meredithcorp.io%2Ff926647f3c831a8bb7a3024468adfd8f%2F1683320006286CKN.jpg&q=60&c=sc&orient=true&poi=auto&h=512" description="Very yummy looking chicken"/>
       </div>
       <Login />
+      <div class="card">
+      <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png" alt="Profile Picture" width="175" height="175"/>
+        <h1>
+          {username}</h1>
+        </div>
     </div>
   );
 }
 
-export default App;
+// map the parts of the state that we want to the props 
+// this refreshes everytime i change the state
+const mapStateToProps = state => ({
+  username: state.username
+})
+
+// use connect() for app to get access to store
+export default connect(mapStateToProps)(App);
--- src/index.js
+++ src/index.js
@@ -3,11 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux'
+import reducer from './reducer';
+
+const store = createStore(reducer);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
@@ -15,3 +23,8 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+
+
+
