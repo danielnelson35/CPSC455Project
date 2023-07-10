947043dd36401d12c3dbba76e26e29324d890473
Jason Song <emailtojasonsong@gmail.com>
2023-07-10 15:16:10 -0700

    Edited connection

--- backend/routes/recipes.js
+++ backend/routes/recipes.js
@@ -1,8 +1,12 @@
-import db from 'mongodb/mongoUtil';
 var express = require('express');
 var router = express.Router();
 
+const connectionURI = "mongodb+srv://teamrecipe:TeamRecipeDB1@sandbox.ykog5qj.mongodb.net/?retryWrites=true&w=majority";
+const dbName = "TeamRecipe";
 const collectionName = "Recipes";
+
+const client = new MongoClient(connectionURI);
+var db = client.db(dbName);
 const collection = db.collection(collectionName);
 
 router.get('/', async (req, res, next) => {
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,8 +1,12 @@
-import db from 'mongodb/mongoUtil';
 var express = require('express');
 var router = express.Router();
 
+const connectionURI = "mongodb+srv://teamrecipe:TeamRecipeDB1@sandbox.ykog5qj.mongodb.net/?retryWrites=true&w=majority";
+const dbName = "TeamRecipe";
 const collectionName = "User";
+
+const client = new MongoClient(connectionURI);
+var db = client.db(dbName);
 const collection = db.collection(collectionName);
 
 router.get('/', (req, res, next) => {
