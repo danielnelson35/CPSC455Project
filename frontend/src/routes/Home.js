408afd3d309c2338eb94261b932f1e7252543474
Daniel Nelson <daniel35@students.cs.ubc.ca>
2023-07-27 15:40:42 -0700

    placeholder drop downs created

--- frontend/src/routes/Home.js
+++ frontend/src/routes/Home.js
@@ -3,6 +3,8 @@ import RecipeCard from 'components/RecipeCard.js';
 import ProfilePreview from 'components/ProfilePreview';
 import GenerateButton from 'components/GenerateButton';
 import LoggedInButtons from 'components/LoggedInButtons';
+import Allergies from 'components/Allergies';
+import Ingredients from 'components/Ingredients';
 import { useSelector } from 'react-redux';
 
 export default function Home() {
@@ -19,6 +21,8 @@ export default function Home() {
           </div>
           <div className='right'>
             <GenerateButton />
+            <Ingredients />
+            <Allergies />
             <RecipeCard />
           </div>
         </div>
